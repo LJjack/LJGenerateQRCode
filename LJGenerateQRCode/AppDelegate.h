da1f2be02f4ff3bb1bf24b93c34537129db0a308
刘俊杰 <945980335@qq.com>
2015-06-02 10:40:32 +0800

    Initial Commit

---
+++ LJGenerateQRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LJGenerateQRCode
+//
+//  Created by liujunjie on 15-6-2.
+//  Copyright (c) 2015年 rj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
